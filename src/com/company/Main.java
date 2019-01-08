c7b7c286d8908fd7c6e1bf8cff41cb88d03da318
Khvashchevskiy Mikhail <cocain1988@gmail.com>
2019-01-08 18:48:26 +0300

    Дополнительный коммит содержащий в себе просто типы данных.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,32 @@
 package com.company;
 
+
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
+        char dataChar       = 'B';
+        int dataInt         = 1234567891;
+        double dataDouble   = 1.5488d;
+        float dataFloat     = 458.254F;
+        String dataString   = "Sthis string data";
+        byte dataByte       = 100;
+        short dataShort     = 12313;
+        boolean dataBoolean = true;
+
 	    System.out.println("Test java 8");
+
+	    System.out.println("This char: "+dataChar);
+        System.out.println("This String: "+dataString);
+        System.out.println("This logical: "+dataBoolean);
+        System.out.println("This Byte: "+dataByte);
+        System.out.println("This Short: "+dataShort);
+        System.out.println("This Int: "+dataInt);
+        System.out.println("This Double: "+dataDouble);
+        System.out.println("This float: "+dataFloat);
+
+
+
     }
 }
