38e7c72d96aaf11547b586a85d59119b41df9b81
Khvashchevskiy Mikhail <cocain1988@gmail.com>
2019-01-08 21:48:51 +0300

    Добавлено перечисление SIZE добавленны нектороые комментарии, а так же работа со строками.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,17 @@
 package com.company;
 
+/**
+ * Это первый комметарий для пробы:
+ * @version 0.0.1 2019-08-01
+ * @author Khvashchevskiy Mikhail
+ *
+ */
 
-import java.util.Arrays;
 
 public class Main {
+
+    public static final String MAX_STRING = "FIX_BUG";
+    public static final Size s = Size.SMALL;
 
     public static void main(String[] args) {
         char dataChar       = 'B';
@@ -14,8 +22,24 @@ public class Main {
         byte dataByte       = 100;
         short dataShort     = 12313;
         boolean dataBoolean = true;
+        String greeting     = "Hello world";
+        String sb           = greeting.substring(0,3);
+        String str          = "Java is a powerfull language!";
+
+        //String all = String.join("/","S", "M", "L", "XL");
 
 	    System.out.println("Test java 8");
+	    System.out.println(greeting);
+	    System.out.println(sb);
+	    //System.out.println(all);
+
+	    str = null;
+
+	    if(str != null && str.length() != 0){
+	        System.out.println("Все работает строка содержит: " +str);
+        }else{
+	        System.out.println("Не отработала проверка на null ");
+        }
 
 	    System.out.println("This char: "+dataChar);
         System.out.println("This String: "+dataString);
