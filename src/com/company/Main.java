cfd14fcb517aa29ab63f0a8b4043c3087e2ee07b
Khvashchevskiy Mikhail <cocain1988@gmail.com>
2019-01-13 17:02:23 +0300

    Работа с классами. Создан класс Employee и так ж ераализован.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,7 @@
 package com.company;
 
 
+import java.time.LocalDate;
 
 /**
  * Создал файлы для нового раздела
@@ -13,6 +14,17 @@ package com.company;
 public class Main {
 
     public static void main(String[] args)  {
+        Employee[] staff = new Employee[3];
+
+        staff[0] = new Employee("Carl Cracker", 7000, 1987, 12, 15);
+        staff[1] = new Employee("Harry Hacker", 5000, 1989, 10, 1 );
+        staff[2] = new Employee("Tony Tester ",  4000, 1990, 3,  15);
+
+        for(Employee e : staff)
+            e.raiseSalary(3);
+
+        for(Employee e: staff)
+            System.out.println("Name = "+ e.getName() +", salary = "+ e.getSalary()+", hireDay = "+ e.getHireDay());
 
     }
 }
