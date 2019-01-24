78d6fb895766b9cd202035dbd36da2fdb274841f
Khvashchevskiy Mikhail <cocain1988@gmail.com>
2019-01-24 22:16:06 +0300

    Применены лямбда функции.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,23 @@
 package com.company;
 
 import java.awt.event.ActionListener;
+import java.util.Arrays;
 //import java.util.Timer;
 import javax.swing.*;
 
 public class Main {
     public static void main(String[] args){
-        //
+        String[] planets = new String[] {
+                "Mercury", "Verus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"
+        };
+        System.out.println(Arrays.toString(planets));
+        System.out.println("Sorted in dictionary order:");
+        Arrays.sort(planets);
+        System.out.println(Arrays.toString(planets));
+        System.out.println("Sorted by length: ");
+        Arrays.sort(planets,
+                (first, second) -> first.length() - second.length());
+        System.out.println(Arrays.toString(planets));
+
     }
 }
