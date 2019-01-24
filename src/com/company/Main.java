896c78af52fed89d6c6f21386f0e93b8f3e8b3e8
Khvashchevskiy Mikhail <cocain1988@gmail.com>
2019-01-24 22:05:18 +0300

    Подгтовка к Лямбда функциям. Проект очищен.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,10 +6,6 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args){
-        ActionListener listener = new TimePrinter();
-        Timer t = new Timer(10000, listener);
-        t.start();
-        JOptionPane.showMessageDialog(null, "Quit program?");
-        System.exit(0);
+        //
     }
 }
