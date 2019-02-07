6b91130a554274163f6f710ee3f4a585bfab2845
Khvashchevskiy Mikhail <cocain1988@gmail.com>
2019-02-07 22:11:10 +0300

    Подготовка к применению знаний в статических внутренних классах.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,33 +1,8 @@
 package com.company;
 
-import java.awt.*;
-import java.awt.event.ActionListener;
-import java.util.Arrays;
-import java.util.Timer;
-//import java.util.Timer;
-import javax.swing.*;
+
 
 public class Main {
-    public static void main(String[] args){
-        TalkingClock clock = new TalkingClock(1000, true);
-        clock.start();
-
-        JOptionPane.showMessageDialog(null, "Quit programm?");
-        System.exit(0);
-
-    }
-
-    @Override
-    public int hashCode() {
-        return super.hashCode();
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        return super.equals(obj);
-
-    }
-
 
 
 }
