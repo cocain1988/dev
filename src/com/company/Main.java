b9f0cd90f495d8f008beeb8658cae67af34f7790
Khvashchevskiy Mikhail <cocain1988@gmail.com>
2019-02-09 18:16:13 +0300

    Подготовка для главы Исключения, утверждения и протоколирование.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,12 +5,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args){
-        double[] d =  new double[20];
-        for(int i = 0; i < d.length; i++)
-            d[i] = 100 * Math.random();
-        ArrayAlg.Pair p = ArrayAlg.minmax(d);
-        System.out.println("min = "+ p.getFirst());
-        System.out.println("max = "+ p.getSecond());
+
     }
 
 }
