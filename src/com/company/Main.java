c67c0c4ec5e6c7a35806a6a3a0a7dfac0656b591
Khvashchevskiy Mikhail <cocain1988@gmail.com>
2019-02-19 21:48:20 +0300

    Проработка темы обобщенного прогпраммирования.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,22 @@
 package com.company;
 
 
-import java.util.logging.Logger;
+import java.time.LocalDate;
+
 
 public class Main {
 
     public static void main(String[] args){
-        Logger.getGlobal().info("Ups...");
-        Logger.getGlobal().warning("Oh is warning");
+        LocalDate[] birthDays = {
+                LocalDate.of(1906, 12, 9),
+                LocalDate.of(1815, 12, 10),
+                LocalDate.of(1903, 12, 3),
+                LocalDate.of(1910, 6, 22),
+        };
 
+        Pair<LocalDate> mm = ArrayAlg.minmax(birthDays);
+        System.out.println("Min = "+ mm.getFirst());
+        System.out.println("Max = "+mm.getSecond());
     }
 
 }
--- src/com/company/MyErrorHendler.java
+++
@@ -1,8 +0,0 @@
-package com.company;
-
-public class MyErrorHendler extends Throwable {
-    public void aVoid (){
-        int x = 1;
-        assert x > 0;
-    }
-}
